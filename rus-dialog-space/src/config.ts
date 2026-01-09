6398606f51e1e035c433bad69947954487741fe0
Ibragimm228 <arimaxgn@gmail.com>
2026-01-09 21:55:25 +0500

    we

--- rus-dialog-space/src/config.ts
+++ rus-dialog-space/src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   apiUrl: import.meta.env.VITE_API_URL || `${window.location.origin}/api`,
   wsUrl: import.meta.env.VITE_WS_URL || `ws://${window.location.host}/ws`,
-  authUrl: 'http://109.172.36.219:8000/api/auth',
+  authUrl: import.meta.env.VITE_AUTH_URL || `${window.location.origin}/api/auth`,
 };
